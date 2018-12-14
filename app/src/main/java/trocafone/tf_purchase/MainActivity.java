0ccb184d83ef1f8e93d81a60666a942cd9b7d861
Santiaso <santiago.aso@trocafone.com>
2018-12-14 11:36:16 -0300

    Create List View with activity

--- app/src/main/java/trocafone/tf_purchase/MainActivity.java
+++ app/src/main/java/trocafone/tf_purchase/MainActivity.java
@@ -1,13 +1,34 @@
 package trocafone.tf_purchase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //* *EDIT* *
+        ListView listview = (ListView) findViewById(R.id.mainList);
+        listview.setOnItemClickListener(this);
+    }
+
+    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+        Log.i("HelloListView", "You clicked Item: " + id + " at position:" + position);
+        // Then you start a new Activity via Intent
+        Intent intent = new Intent();
+        intent.setClass(this, ListItemDetail.class);
+        intent.putExtra("position", position);
+        // Or / And
+        intent.putExtra("id", id);
+        startActivity(intent);
     }
 }
