046b72a7b8d4437a951c5918901e8cd9759feb1d
Santiaso <santiago.aso@trocafone.com>
2018-12-14 11:06:36 -0300

    Initialized project

---
+++ app/src/main/java/trocafone/tf_purchase/MainActivity.java
@@ -0,0 +1,13 @@
+package trocafone.tf_purchase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
